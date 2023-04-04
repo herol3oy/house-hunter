58273435df5b547aa17d559f6755f1c311b29250
Hamed <hamedsedighi@gmail.com>
2023-04-04 15:12:28 +0200

    display bars icon in top bar

--- components/topBar.tsx
+++ components/topBar.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import Image from "next/image";
 
 export default function TopBar() {
+  const [showNavigation, setShowNavigation] = useState(false);
+
+  const toggleNav = () => {
+    setShowNavigation(!showNavigation);
+  };
+
   return (
-    <nav className="flex flex-col md:flex-row items-center bg-blue-accent p-10 gap-10 md:px-60 w-full">
+    <nav className="bg-blue-accent flex flex-col md:flex-row justify-between items-center gap-5 md:gap-10 md:px-60 p-10 w-full">
       <Image
         src="/reality-properties-logo.png"
         alt="Reality Properties"
         width={161}
         height={42}
       />
-      <div className="flex-1 md:flex flex items-center flex-col md:flex-row justify-start gap-2 md:gap-10">
+      <div
+        className={`flex-1 flex items-center flex-col md:flex-row justify-start gap-2 md:gap-10 ${
+          !showNavigation ? "hidden" : "block"
+        } md:flex`}
+      >
         {NAVIGATION.map((nav) => (
           <a key={nav.name} href={nav.href} className="text-slate-50">
             {nav.name}
@@ -19,6 +30,7 @@ export default function TopBar() {
       <div className="flex">
         {NAVIGATION_FEATURES.map((feature) => (
           <Image
+            className={`md:block ${!showNavigation ? "hidden" : "md:block"}`}
             key={feature.name}
             src={feature.src}
             alt={feature.name}
@@ -27,6 +39,14 @@ export default function TopBar() {
           />
         ))}
       </div>
+      <Image
+        className="md:hidden"
+        src={`/icons/${showNavigation ? "close" : "bars"}.svg`}
+        alt={`${showNavigation ? "close" : "bars"} icon`}
+        width={30}
+        height={30}
+        onClick={toggleNav}
+      />
     </nav>
   );
 }
