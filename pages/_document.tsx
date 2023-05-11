15cc524e858f510383c1551a679c7da31ac07d8e
demah <hamedsedighi@gmail.com>
2023-05-11 12:38:56 +0200

    add site title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>House Hunter | Find the house of your dreams</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
